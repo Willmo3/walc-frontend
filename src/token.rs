0502b1cf8701c6b93ddd520ce83d6531fd461db8
willmo3 <thelordofbouge@gmail.com>
2024-03-23 12:02:26 -0400

    Add early lexer -- need negative num support

--- src/token.rs
+++ src/token.rs
@@ -1,14 +1,22 @@
 // Basic tree-based calculator.
 // Author: Willmo3
 
-pub enum TokenType {
-    Number, // Coerce all numbers to floats
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Lexeme {
+    Number { value: f64 }, // Coerce all numbers to floats
+    OpenParen,
+    CloseParen,
     Plus,
     Minus,
     Star,
     Slash,
+    // Special token that all files are terminated by
+    EOF,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Token {
     Number { value: f64 },
     Add { left: Box<Token>, right: Box<Token> },
@@ -20,5 +28,10 @@ pub enum Token {
 // Since this is just a frontend, don't do any evaluation!
 // Simply export the abstract syntax tree to JSON
 impl Token {
-
+    pub fn export(&self) -> String {
+        serde_json::to_string(&self).unwrap()
+    }
+    pub fn import(data: &str) -> Self {
+        serde_json::from_str(data).unwrap()
+    }
 }
