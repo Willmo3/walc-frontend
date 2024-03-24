d5e45201b47f89bfd20660cfdb6b7915e008cd13
willmo3 <thelordofbouge@gmail.com>
2024-03-24 12:25:33 -0400

    Add basic main fn driver

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use std::io::stdin;
+use crate::lexer::lex;
+use crate::parser::parse;
+
 mod token;
 mod lexer;
 mod parser;
 
+// Very simple calculator driver.
+// Basically no error checking
 fn main() {
-    println!("Hello, world!");
+    // Repeatedly read lines from stdin
+    // Repeatedly write answers to stdout
+    let mut buffer = String::new();
+    while stdin().read_line(&mut buffer).unwrap() > 0 {
+        println!("{}", parse(lex(&buffer)).unwrap().evaluate());
+        buffer = String::new();
+    }
 }
-
-// Walc
-// -- multiple frontends
-// -- one library, invoked via wasm
-// -- can it be run natively?
-//      -- First step: build frontend
-//      -- Next step: build backend
-//      -- Last step: frontend frontend?
